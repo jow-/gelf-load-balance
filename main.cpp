0a96f1f00d29765175262789f283405ef80fe220
Jo-Philipp Wich <jo@mein.io>
2017-06-14 14:31:38 +0200

    udp_server: use separate UDP socket for sending
    In order to properly support using the same UDP port for frontend and
    backend connections, use a separate send-only UDP for upstream communication.
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <functional>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include <boost/property_tree/ptree.hpp>  
+#include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/foreach.hpp>
 #include <boost/bind.hpp>
@@ -13,53 +13,54 @@
 #include "upstream.hpp"
 
 using namespace boost::asio::ip;
-using namespace boost::property_tree; 
+using namespace boost::property_tree;
 
-namespace UdpAsyService  
-{  
-    class udp_server  
-    {  
-    public:  
+namespace UdpAsyService
+{
+    class udp_server
+    {
+    public:
         udp_server(
             boost::asio::io_service& io,
             udp::endpoint endpoint,
-            ptree& config)  
+            ptree& config)
             :socket_(io, endpoint),
+             send_socket_(io, udp::endpoint(udp::v4(), 0)),
              protocol_(io, boost::bind(&udp_server::hand_chunk, this, _1), config),
              upstream_(config)
         {
-            start_recive();  
-        }
-  
-        void start_recive()  
-        {  
-            socket_.async_receive_from(boost::asio::buffer(rec_buf_), remot_endpoint_,  
-                boost::bind(&udp_server::hand_receive, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));  
+            start_recive();
         }
 
-        void hand_chunk(BufferStack buffer_stack) 
+        void start_recive()
+        {
+            socket_.async_receive_from(boost::asio::buffer(rec_buf_), remot_endpoint_,
+                boost::bind(&udp_server::hand_receive, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+        }
+
+        void hand_chunk(BufferStack buffer_stack)
         {
             udp::endpoint &up_endpoint = upstream_.get();
             std::for_each(buffer_stack.begin(), buffer_stack.end(),[&](SharedBuffer& sb){
-                socket_.async_send_to(boost::asio::buffer(*(sb.buff), sb.size), up_endpoint,
+                send_socket_.async_send_to(boost::asio::buffer(*(sb.buff), sb.size), up_endpoint,
                       boost::bind(&udp_server::hand_send, this, sb.buff, sb.size, boost::asio::placeholders::error,
                             boost::asio::placeholders::bytes_transferred));
             });
 
         }
-  
-    private:  
-        void hand_receive(const boost::system::error_code& error, std::size_t size)  
-        {  
-            if (!error || error != boost::asio::error::message_size)  
+
+    private:
+        void hand_receive(const boost::system::error_code& error, std::size_t size)
+        {
+            if (!error || error != boost::asio::error::message_size)
             {
                 SharedBufferInner copy = SharedBufferInner(new Buffer());
                 boost::swap(rec_buf_, *copy);
                 if ((*copy)[0] == 0x78 || (*copy)[0] == 0x1f) {
                     udp::endpoint &up_endpoint = upstream_.get();
-                    socket_.async_send_to(boost::asio::buffer(*copy, size), up_endpoint,
+                    send_socket_.async_send_to(boost::asio::buffer(*copy, size), up_endpoint,
                         boost::bind(&udp_server::hand_send, this, copy, size, boost::asio::placeholders::error,
-                            boost::asio::placeholders::bytes_transferred));  
+                            boost::asio::placeholders::bytes_transferred));
                 } else if ((*copy)[0] == 0x1e) {
                     SharedBuffer sb;
                     sb.size = size;
@@ -71,34 +72,35 @@ namespace UdpAsyService
         }
 
         void hand_send(SharedBufferInner buff, size_t buff_len, const boost::system::system_error& error, std::size_t size)
-        {  
+        {
 //            std::cout << "send byte: " << buff_len << std::endl;
-        }  
-  
-    private:  
-        udp::socket socket_;  
+        }
+
+    private:
+        udp::socket socket_;
+        udp::socket send_socket_;
         udp::endpoint remot_endpoint_;
         Buffer rec_buf_;
         Protocol protocol_;
         Upstream upstream_;
-    };  
-  
+    };
+
     void udp_asy_server(
             udp::endpoint endpoint,
-            ptree& config)  
-    {  
+            ptree& config)
+    {
         try
         {
-            boost::asio::io_service io;  
-            udp_server server(io, endpoint, config);  
-  
-            io.run();  
+            boost::asio::io_service io;
+            udp_server server(io, endpoint, config);
+
+            io.run();
         }
         catch (std::exception& e)
         {
             std::cerr << e.what() << std::endl;
         }
-    }  
+    }
 }
 
 int main(int argc, char* argv[])

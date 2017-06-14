4b97f9872720fe3d381d9a25921e7e35d8dc85c1
Jo-Philipp Wich <jo@mein.io>
2017-06-14 10:29:01 +0200

    define: increase maximum message size to 16K
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- define.h
+++ define.h
@@ -1,7 +1,7 @@
 #ifndef __GELF_LOAD_BALANCE_DEFINE_H__
 #define __GELF_LOAD_BALANCE_DEFINE_H__
 
-#define MAX_SIZE 1500
+#define MAX_SIZE 16384
 #define DEFAULT_CHUNKED_TIMEOUT 5
 
 typedef boost::array<char, MAX_SIZE> Buffer;
